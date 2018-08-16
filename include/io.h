ddde6b3aae41b4f654ff78b166aace1607e2f85d
blueadark <1532249554@qq.com>
2018-08-16 16:25:47 +0800

    提交2

---
+++ include/io.h
@@ -0,0 +1,13 @@
+/*
+ * io.h
+ *
+ *  Created on: Mar. 12, 2015
+ *      Author: chenxm
+ */
+#ifndef IO_H_
+#define IO_H_
+
+void flow_print(const flow_t *flow);
+void save_flow_json(const flow_t *flow, const char* file);
+
+#endif
